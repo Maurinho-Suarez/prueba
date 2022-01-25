4f85de0ea6cb02e5c5c9e2321d13bba6a53df769
maurinho <suarezmauricio20@gmail.com>
2022-01-25 19:59:19 -0300

    agregando nuevo archivo en views/formulario.html

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 
 const app = express();
+const router = express.Router();
 
 //app.get('/', (req,res) => {
   //  res.send("Hola Maurinho!!");
 //});
 
+//app.use();
+
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+    res.sendFile(path.join(__dirname, '/views/formulario.html'));
 });
 
 app.listen(3000, (req,res) => {
